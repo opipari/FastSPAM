4eced4e3bd6160d8b3aa523680269f80108c0283
topipari <topipari@amazon.com>
2023-08-14 12:55:16 -0700

    Moving experiments folder.

--- video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_automatic.sh
+++ video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_automatic.sh
@@ -10,13 +10,14 @@ source ./envs/segment-anything/bin/activate
 pip install -r ./requirements/segment-anything.txt
 
 echo "Downloading data"
-cd video_panoptic_segmentation/datasets/MVPd && \
+cd video_panoptic_segmentation/datasets/MVPd
 ./data/download.sh -s val
 cd ../../..
 
 
 # Download pre-trained model
-wget -P ./zero_shot_scene_segmentation/models/segment-anything/segment-anything/checkpoints/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+aws s3 cp s3://prism-intern-anthony/models/segment-anything/pretrained/sam_vit_h_4b8939.pth ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/
+# wget -P ./zero_shot_scene_segmentation/models/segment-anything/segment-anything/checkpoints/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 
 # python video_panoptic_segmentation/segment-anything/evaluate_automatic.py -out 
 # tar -cf 
