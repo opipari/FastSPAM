1b9046ec59ccb0ddde49501a9e9592c7bb611ebf
topipari <topipari@amazon.com>
2023-08-15 08:22:55 -0700

    Updating download in script.

--- video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_automatic.sh
+++ video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_automatic.sh
@@ -1,20 +1,23 @@
 EXPERIMENT_NAME="evaluate_pretrained_sam_automatic"
 OUTPUT_DIR="./video_panoptic_segmentation/results/segment-anything/"
 
-echo "Setting up virtualenvironment"
-# Setup virtualenvironment
-python3.8 -m venv ./envs/segment-anything
-source ./envs/segment-anything/bin/activate
-pip install -r ./requirements/segment-anything.txt
+# Download pre-trained model
+/opt/amazon/bin/aws s3 cp s3://prism-intern-anthony/models/segment-anything/pretrained/sam_vit_h_4b8939.pth ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/
 
 echo "Downloading data"
 cd video_panoptic_segmentation/datasets/MVPd
 ./data/download.sh -s val
 cd ../../..
 
+echo "Setting up virtualenvironment"
+# Setup virtualenvironment
+python3.8 -m venv ./envs/segment-anything
+source ./envs/segment-anything/bin/activate
+pip install -r ./requirements/segment-anything.txt
 
-# Download pre-trained model
-aws s3 cp s3://prism-intern-anthony/models/segment-anything/pretrained/sam_vit_h_4b8939.pth ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/
+
+
+
 # wget -P ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 
 echo "Starting evaluation"
