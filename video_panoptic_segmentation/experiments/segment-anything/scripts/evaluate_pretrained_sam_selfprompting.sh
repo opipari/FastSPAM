cd3e13e58c34f64610b27e08473d465637aea5f7
topipari <topipari@amazon.com>
2023-08-16 09:58:05 -0700

    Updating scripts for sam.

--- video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_selfprompting.sh
+++ video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_selfprompting.sh
@@ -1,6 +1,5 @@
-
 EXPERIMENT_NAME="evaluate_pretrained_sam_selfprompting"
-OUTPUT_DIR="./video_panoptic_segmentation/results/segment-anything/"
+OUTPUT_DIR="video_panoptic_segmentation/models/segment-anything/results"
 
 echo "Setting up virtualenvironment"
 # Setup virtualenvironment
@@ -14,14 +13,19 @@ cd video_panoptic_segmentation/datasets/MVPd
 cd ../../..
 
 # Download pre-trained model
-aws s3 cp s3://vesta-intern-anthony/models/segment-anything/pretrained/sam_vit_h_4b8939.pth ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/
+aws s3 cp s3://vesta-intern-anthony/video_panoptic_segmentation/models/segment-anything/pretrained/sam_vit_h_4b8939.pth ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/
 # wget -P ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 
 echo "Starting evaluation"
-python video_panoptic_segmentation/models/segment-anything/evaluate_selfprompting.py --config-path ./video_panoptic_segmentation/experiments/segment-anything/configs/evaluate_pretrained_sam_selfprompting.json --output-path ${OUTPUT_DIR}
+python video_panoptic_segmentation/models/segment-anything/evaluate_selfprompting.py \
+		--config-path ./video_panoptic_segmentation/experiments/segment-anything/configs/evaluate_pretrained_sam_selfprompting.json \
+		--output-path ${OUTPUT_DIR}
+
+# Save status of repository for reference
+git log -1 --oneline > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/repo_state.txt
 
 echo "Compressing results"
 tar -C ${OUTPUT_DIR} -cf ${EXPERIMENT_NAME}.tar.gz ${EXPERIMENT_NAME}/
-aws s3 cp ${EXPERIMENT_NAME}.tar.gz s3://vesta-intern-anthony/results/segment-anything/
+aws s3 cp ${EXPERIMENT_NAME}.tar.gz s3://vesta-intern-anthony/${OUTPUT_DIR}/
 
 echo "Finished"
