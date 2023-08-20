8828d962f52ce82999ea93ee08909dabf0031fc2
topipari <topipari@amazon.com>
2023-08-19 17:55:34 -0700

    New upload for grid test.

--- video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_automatic.sh
+++ video_panoptic_segmentation/experiments/segment-anything/scripts/evaluate_pretrained_sam_automatic.sh
@@ -1,6 +1,9 @@
 EXPERIMENT_NAME="evaluate_pretrained_sam_automatic"
 OUTPUT_DIR="video_panoptic_segmentation/models/segment-anything/results"
 
+# Save status of repository for reference
+git log -1 --oneline > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/repo_state.txt
+
 echo "Setting up virtualenvironment"
 # Setup virtualenvironment
 python3.8 -m venv ./envs/segment-anything
@@ -22,12 +25,11 @@ python video_panoptic_segmentation/models/segment-anything/evaluate_automatic.py
 		--config-path ./video_panoptic_segmentation/experiments/segment-anything/configs/evaluate_pretrained_sam_automatic.json \
 		--output-path ${OUTPUT_DIR}
 
-# Save status of repository for reference
-git log -1 --oneline > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/repo_state.txt
 
 echo "Compressing results"
 tar -C ${OUTPUT_DIR} -cf ${EXPERIMENT_NAME}.tar.gz ${EXPERIMENT_NAME}/
 echo "Uploading results"
-aws s3 cp ${EXPERIMENT_NAME}.tar.gz s3://vesta-intern-anthony/${OUTPUT_DIR}/ > /dev/null
+uploader=/opt/amazon/compute_grid_utils/output_uploader
+$uploader ${EXPERIMENT_NAME}.tar.gz
 
 echo "Finished"
