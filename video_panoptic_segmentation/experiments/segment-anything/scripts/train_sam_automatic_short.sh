bd5f907b309de32c256d3c39bcb19ee90f368206
topipari <topipari@amazon.com>
2023-09-01 18:28:14 -0700

    Adding script to train sam with less steps.

---
+++ video_panoptic_segmentation/experiments/segment-anything/scripts/train_sam_automatic_short.sh
@@ -0,0 +1,46 @@
+CONFIG_FILE="video_panoptic_segmentation/models/segment-anything/configs/train_automatic_sam_vit_b_short.py"
+
+
+EXPERIMENT_NAME="$(python ${CONFIG_FILE} experiment_name)"
+OUTPUT_DIR="$(python ${CONFIG_FILE} output_dir)"
+
+mkdir -p ${OUTPUT_DIR}/${EXPERIMENT_NAME}
+
+echo "Setting up virtualenvironment"
+# Setup virtualenvironment
+python3.8 -m venv ./envs/segment-anything
+source ./envs/segment-anything/bin/activate
+pip install -r ./requirements/segment-anything.txt
+
+echo "Downloading data"
+cd video_panoptic_segmentation/datasets/MVPd
+./data/download.sh -s train -d panomasksRGB -d imagesRGB.0000000000
+./data/download.sh -s val -d panomasksRGB -d imagesRGB.0000000000
+cd ../../..
+
+# Download pre-trained model
+echo "Downloading pretrained model"
+aws s3 cp s3://vesta-intern-anthony/video_panoptic_segmentation/models/segment-anything/pretrained/sam_vit_b_01ec64.pth ./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/ > /dev/null
+
+nvidia-smi > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/nvidia_smi.txt
+
+echo "Starting evaluation"
+python video_panoptic_segmentation/models/segment-anything/train_automatic.py \
+		--config-path ${CONFIG_FILE}
+
+# Save status of repository for reference
+git log -1 --oneline > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/repo_state.txt
+
+echo "The experiment script executed is:" > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/script_state.txt
+echo "basename: [$(basename "$0")]" >> ${OUTPUT_DIR}/${EXPERIMENT_NAME}/script_state.txt
+echo "dirname : [$(dirname "$0")]" >> ${OUTPUT_DIR}/${EXPERIMENT_NAME}/script_state.txt
+echo "pwd     : [$(pwd)]" >> ${OUTPUT_DIR}/${EXPERIMENT_NAME}/script_state.txt
+
+
+echo "Compressing results"
+tar -C ${OUTPUT_DIR} -cf ${EXPERIMENT_NAME}.tar.gz ${EXPERIMENT_NAME}/
+echo "Uploading results"
+uploader=/opt/amazon/compute_grid_utils/output_uploader
+$uploader ${EXPERIMENT_NAME}.tar.gz
+
+echo "Finished"
---
+++ video_panoptic_segmentation/models/segment-anything/configs/train_automatic_sam_vit_b_short.py
@@ -0,0 +1,26 @@
+
+class InitConfig():
+    def __init__(self):
+        self.experiment_name = "train_automatic_sam_vit_b_short"
+        self.output_dir = "video_panoptic_segmentation/models/segment-anything/results"
+        self.model_type = "vit_b"
+        self.sam_checkpoint = "./video_panoptic_segmentation/models/segment-anything/segment-anything/checkpoints/sam_vit_b_01ec64.pth"
+        
+        self.use_augmentation = False
+        self.total_iterations = 45000
+        self.eval_every = 5000
+        self.eval_iterations = 50
+        self.learning_rate = 8e-4
+        self.adam_betas = (0.9, 0.999)
+        self.weight_decay = 0.1
+        self.warmup_iters = 250
+        self.lr_decay_iters = [30000, 43333]
+        self.num_mask_samples = 16
+        self.min_box_area_pcnt = 0.001
+        self.seed = 0
+
+if __name__=="__main__":
+    import sys
+
+    if len(sys.argv)==2:
+        print(getattr(InitConfig(), sys.argv[1]))
