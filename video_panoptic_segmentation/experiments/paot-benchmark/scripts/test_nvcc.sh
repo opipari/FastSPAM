e0a3227af394d5fa0ae49f095d0f0d4c2b201f1b
Anthony Opipari <topipari@umich.edu>
2023-09-25 13:04:33 -0700

    Fixing test.

--- video_panoptic_segmentation/experiments/paot-benchmark/scripts/test_nvcc.sh
+++ video_panoptic_segmentation/experiments/paot-benchmark/scripts/test_nvcc.sh
@@ -1,13 +1,6 @@
 EXPERIMENT_NAME="train_r50"
 OUTPUT_DIR="results"
 
-echo "Setting up virtualenvironment"
-# Setup virtualenvironment
-python3.8 -m venv ./envs/paot-benchmark
-source ./envs/paot-benchmark/bin/activate
-pip install -r ./requirements/paot-benchmark/base.txt
-pip install -r ./requirements/paot-benchmark/deps.txt
-
 echo "Testing nvcc before toolkit install"
 echo $(nvcc -C)
 
@@ -16,5 +9,14 @@ sudo apt install nvidia-cuda-toolkit
 
 echo "Testing nvcc after toolkit install"
 echo $(nvcc -C)
+
+echo "Setting up virtualenvironment"
+# Setup virtualenvironment
+python3.8 -m venv ./envs/paot-benchmark
+source ./envs/paot-benchmark/bin/activate
+pip install -r ./requirements/paot-benchmark/base.txt
+pip install -r ./requirements/paot-benchmark/deps.txt
+
+
 
 echo "Finished"
