953329233920a43e7886019c978630f63c48fc5b
Anthony Opipari <topipari@umich.edu>
2023-09-25 12:55:10 -0700

    Adding test script to ensure nvcc is there.

---
+++ video_panoptic_segmentation/experiments/paot-benchmark/scripts/test_nvcc.sh
@@ -0,0 +1,20 @@
+EXPERIMENT_NAME="train_r50"
+OUTPUT_DIR="results"
+
+echo "Setting up virtualenvironment"
+# Setup virtualenvironment
+python3.8 -m venv ./envs/paot-benchmark
+source ./envs/paot-benchmark/bin/activate
+pip install -r ./requirements/paot-benchmark/base.txt
+pip install -r ./requirements/paot-benchmark/deps.txt
+
+echo "Testing nvcc before toolkit install"
+echo $(nvcc -C)
+
+echo "Installing nvidia toolkit"
+sudo apt install nvidia-cuda-toolkit 
+
+echo "Testing nvcc after toolkit install"
+echo $(nvcc -C)
+
+echo "Finished"
