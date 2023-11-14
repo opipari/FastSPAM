c79383b67969ed8a16efeb14ef3dd5538534f9c9
Anthony Opipari <topipari@gmail.com>
2023-11-14 10:29:30 -0500

    Adding test script for swinb video k net.

---
+++ video_panoptic_segmentation/experiments/Video-K-Net/scripts/test_instance_swinb.sh
@@ -0,0 +1,46 @@
+EXPERIMENT_NAME="test_instance_swinb"
+OUTPUT_DIR="video_panoptic_segmentation/models/Video-K-Net/Video-K-Net/results"
+
+# Save status of repository for reference
+git log -1 --oneline > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/repo_state.txt
+
+
+pip install --upgrade pip
+pip install Cython==3.0.2
+pip install -r ./requirements/video-k-net/video-k-net.txt
+
+
+cd video_panoptic_segmentation/datasets/MVPd
+# ./data/download.sh -s train -m -d imagesRGB.0000000000 -d panomasksRGB
+# ./data/download.sh -s val -m -d imagesRGB.0000000000 -d panomasksRGB
+./data/download.sh -s test -m -d imagesRGB.0000000000 -d panomasksRGB
+
+cd ../../..
+
+mkdir ./video_panoptic_segmentation/models/Video-K-Net/Video-K-Net/data/
+ln -s $PWD/video_panoptic_segmentation/datasets/MVPd/MVPd ./video_panoptic_segmentation/models/Video-K-Net/Video-K-Net/data/
+
+cd video_panoptic_segmentation/models/Video-K-Net/Video-K-Net
+
+
+# ln -s $PWD/video_panoptic_segmentation/datasets/youtube_vis_2019 ./video_panoptic_segmentation/models/Video-K-Net/Video-K-Net/data/
+# CONFIG="configs/video_knet_vis/video_knet_vis/knet_track_r50_1x_youtubevis.py"
+
+mkdir results
+aws s3 cp s3://vesta-intern-anthony/video_panoptic_segmentation/models/Video-K-Net/trained/instance_models/iter_200000.pth ./results/ > /dev/null
+
+
+CONFIG="configs/video_knet_vis/video_knet_vis/knet_track_swinb_deformable_1x_mvpdvis.py"
+WORK_DIR="./results"
+CHECKPOINT="./results/iter_200000.pth"
+GPUS="1"
+PORT=${PORT:-$((29500 + $RANDOM % 29))}
+PYTHONPATH=$PYTHONPATH:./ python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT ./tools_vis/test_whole_video.py $CONFIG $CHECKPOINT --launcher pytorch --work-dir=${WORK_DIR} --format-only
+
+echo "Compressing results"
+tar -cf ${EXPERIMENT_NAME}.tar.gz ${WORK_DIR}/
+echo "Uploading results"
+uploader=/opt/amazon/compute_grid_utils/output_uploader
+$uploader ${EXPERIMENT_NAME}.tar.gz
+
+echo "Finished"
