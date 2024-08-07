1179d6401958a20fe16f256f403f7a6b83e76bbf
Anthony Opipari <topipari@gmail.com>
2024-08-07 11:28:40 -0700

    Code for detectron2 format.

---
+++ video_segmentation/experiments/mvpd2detectron_1.sh
@@ -0,0 +1,27 @@
+EXPERIMENT_NAME="train"
+OUTPUT_DIR="./work_dirs"
+
+
+cd video_segmentation/datasets/MVPd
+mkdir ./work_dirs
+
+
+pip install --upgrade pip
+pip install -r ./requirements/mvpd.txt
+
+./data/download.sh -s train -p -m -d imagesRGB.0000000000 -d panomasksRGB
+# ./data/download.sh -s val -m -d imagesRGB.0000000000 -d panomasksRGB
+
+
+python convert2detectron.py --root_path MVPd --split train
+
+mv MVPd_train_detectron2.json ./work_dirs/
+
+
+echo "Compressing results"
+tar -cf ${EXPERIMENT_NAME}.tar.gz ${OUTPUT_DIR}/
+echo "Uploading results"
+uploader=/opt/amazon/compute_grid_utils/output_uploader
+$uploader ${EXPERIMENT_NAME}.tar.gz
+
+echo "Finished"
