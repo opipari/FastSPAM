aceb5a644f738e8a821de1535dabc588d9310e11
Anthony Opipari <topipari@gmail.com>
2024-08-13 09:07:09 -0700

    Adding prep scripts for updated omg seg dataset.

--- video_segmentation/experiments/OMG-Seg/train_prep_3.sh
+++ video_segmentation/experiments/OMG-Seg/train_prep_3.sh
@@ -13,7 +13,7 @@ pip install -r ./requirements/omg-seg/omg-seg.txt
 pip install yapf==0.32
 
 cd video_segmentation/datasets/MVPd
-bash ./data/download_4.sh -s train -m -d imagesRGB.0000000000 -d panomasksRGB
+bash ./data/download_3.sh -s train -m -d imagesRGB.0000000000 -d panomasksRGB
 # ./data/download.sh -s val -m -d imagesRGB.0000000000 -d panomasksRGB
 
 
@@ -25,9 +25,9 @@ ln -s $PWD/video_segmentation/datasets/MVPd/MVPd ./video_segmentation/models/OMG
 cd video_segmentation/models/OMG-Seg/OMG-Seg
 
 
-# ./tools/dist.sh gen_cls seg/configs/m2ov_val/eval_m2_convl_300q_ov_mvpd.py 1
+./tools/dist.sh gen_cls seg/configs/m2ov_val/eval_m2_convl_300q_ov_mvpd.py 1
 # ./tools/dist.sh train seg/configs/m2ov_train/omg_convl_vlm_fix_12e_ov_mvpd.py 8
-./tools/dist.sh train seg/configs/m2_train_close_set/omg_convl_mvpd_short.py 1
+./tools/dist.sh train seg/configs/m2ov_train/omg_convl_vlm_fix_12e_ov_mvpd_short.py 1
 
 mv data/MVPd/train_annotations.json ./work_dirs/
 
