65c7628b16f1d860a7fe5fe4aedbc2688bf504b5
Anthony Opipari <topipari@gmail.com>
2024-08-22 14:27:50 -0700

    fixing name.

--- video_segmentation/experiments/LVVIS/prep_data3.sh
+++ video_segmentation/experiments/LVVIS/prep_data3.sh
@@ -12,7 +12,7 @@ cd video_segmentation/datasets/MVPd
 xargs -L 1 pip install < ./requirements/mvpd.txt
 
 
-bash ./data/download_3.sh -s val -m -d imagesRGB.0000000000 -d panomasksRGB
+bash ./data/download_3.sh -s test -m -d imagesRGB.0000000000 -d panomasksRGB
 
 
 python convert2detectron.py --root_path MVPd --split test
