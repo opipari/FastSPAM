0010926222ace32c898448bd707e7b00894da3d0
Anthony Opipari <topipari@gmail.com>
2024-08-22 11:32:09 -0700

    Adding prep data scripts.

---
+++ video_segmentation/experiments/LVVIS/prep_data1.sh
@@ -0,0 +1,30 @@
+EXPERIMENT_NAME="train"
+OUTPUT_DIR="output"
+
+mkdir -p ${OUTPUT_DIR}/${EXPERIMENT_NAME}
+# Save status of repository for reference
+git log -1 --oneline > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/repo_state.txt
+
+
+pip install --upgrade pip
+
+cd video_segmentation/datasets/MVPd
+pip install -r ./requirements/mvpd.txt
+
+
+bash ./data/download_1.sh -s test -m -d imagesRGB.0000000000 -d panomasksRGB
+
+
+python convert2detectron.py --root_path MVPd --split test
+
+mv MVPd_test_detectron2.json output
+
+WORK_DIR="./output"
+
+echo "Compressing results"
+tar -cf ${EXPERIMENT_NAME}.tar.gz ${WORK_DIR}/
+echo "Uploading results"
+uploader=/opt/amazon/compute_grid_utils/output_uploader
+$uploader ${EXPERIMENT_NAME}.tar.gz
+
+echo "Finished"
