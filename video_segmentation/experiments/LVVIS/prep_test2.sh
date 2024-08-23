9e310940123c7614d0ca47705e97daaa4162317a
Anthony Opipari <topipari@gmail.com>
2024-08-22 19:18:26 -0700

    Adding scripts for test dataset in coco.

---
+++ video_segmentation/experiments/LVVIS/prep_test2.sh
@@ -0,0 +1,32 @@
+EXPERIMENT_NAME="train"
+OUTPUT_DIR="output"
+
+mkdir -p ${OUTPUT_DIR}/${EXPERIMENT_NAME}
+# Save status of repository for reference
+git log -1 --oneline > ${OUTPUT_DIR}/${EXPERIMENT_NAME}/repo_state.txt
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+python -m pip install --upgrade pip
+
+cd video_segmentation/datasets/MVPd
+xargs -L 1 python -m pip install < ./requirements/mvpd.txt
+python -m pip install tqdm
+
+bash ./data/download_2.sh -s test -m -d imagesRGB.0000000000 -d panomasksRGB
+
+
+python convert2coco.py --root_path MVPd --split test
+
+mv MVPd_test_coco.json output
+
+WORK_DIR="./output"
+
+echo "Compressing results"
+tar -cf ${EXPERIMENT_NAME}.tar.gz ${WORK_DIR}/
+echo "Uploading results"
+uploader=/opt/amazon/compute_grid_utils/output_uploader
+$uploader ${EXPERIMENT_NAME}.tar.gz
+
+echo "Finished"
