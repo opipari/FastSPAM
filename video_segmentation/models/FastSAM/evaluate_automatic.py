ac07460a60e488d524db69793e524626c218bd42
Anthony Opipari <topipari@gmail.com>
2024-09-12 03:04:29 +0000

    Changes for experiment on original 480x640 resolution input and 10% sample of videos less than 250.

--- video_segmentation/models/FastSAM/evaluate_automatic.py
+++ video_segmentation/models/FastSAM/evaluate_automatic.py
@@ -10,7 +10,7 @@ from PIL import Image
 import numpy as np
 import torch
 
-from fvcore.nn import FlopCountAnalysis
+#from fvcore.nn import FlopCountAnalysis
 
 from panopticapi.utils import id2rgb
 
@@ -18,7 +18,7 @@ from MVPd.utils.MVPdataset import MVPDataset, MVPVideo, MVPdCategories, video_co
 
 from fastsam import FastSAM, FastSAMPrompt
 
-from video_panoptic_segmentation.metrics import utils as metric_utils
+from video_segmentation.metrics import utils as metric_utils
 
 
 def get_dataset(dataset_config):
@@ -47,10 +47,27 @@ def evaluation_process(index, nprocs, config, output_dir):
         inds = list(range(i_start, i_end))
         dataset = torch.utils.data.Subset(dataset, inds)
         print(len(dataset))
-
+    
+    rand_inds = [1367,  100,  983,   83, 1586,  896,  683, 1598, 1092, 1020,  435,  747,
+        1497,  484,  473,  367,  622,   14, 1290, 1414,  459,  740,  111, 1383,
+        1189, 1698, 1280, 1584,  286,  229, 1279,  463, 1396, 1305,  500,  214,
+        1513,  108,  536, 1729,  240, 1184, 1061,  722,  513,  650,   59,  128,
+         180, 1028,   96,  970, 1488, 1470, 1128,  615,  274, 1155,  519,  280,
+         938, 1547,  234,  971,  620,  677,  659,  227, 1135, 1001,  150,  586,
+         825, 1541,    4,  178, 1285,  209,  966,  153,  857,  580,  633,  954,
+        1520,  852, 1580,  348, 1457,  223,  205,  824,   56,  511, 1400,  879,
+        1632,  116, 1007, 1145,  717, 1207, 1164,  772, 1002, 1315, 1114, 1206,
+        1011,   44, 1374, 1505, 1269, 1049,  351,  373, 1510,  177, 1427, 1193,
+         853,   41, 1583,  648, 1391,  829, 1485, 1682, 1362,   64, 1328, 1262,
+        1228, 1499, 1492,  778, 1641, 1297,  791, 1349,  483, 1168, 1324, 1201,
+         979,   88, 1010, 1590, 1208, 1132, 1714,  173,  893, 1460,  477, 1096,
+         847,  216, 1601,  624,  119,  667,  527,  237,  221, 1496,  322,  989,
+        1614,  329,  305,  122, 1401,  251]
     # print("Within evaluation process")
     with torch.no_grad():
         for vi, video in enumerate(tqdm(dataset, position=0, disable=index!=0)):
+            if vi not in rand_inds:
+                continue
             # vitime = time.time()
             first_sample = next(iter(video))
             video_name = first_sample['meta']['video_name']
